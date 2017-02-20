58cb338d8e480918943646d003d8214190f5fd94
Sam Delacruz <sdelacruz@samdlc.local>
2017-02-20 23:49:02 +0000

    Add Games[] caching, and cosmetic / standards changes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,35 +1,38 @@
+# Top level game class, representing overall single game state
+# Class is store for game objects
 class Game
   private_class_method :new
   attr_reader :id, :board, :score, :game_over
 
-  @@games = []
+  @games = []
 
   class << self
+    attr_reader :games
     def start
       game_props = {
         id: next_id,
         board: Game::Board.random,
         score: 0,
-        game_over: false,
+        game_over: false
       }
 
       new game_props
     end
 
     def next_id
-      @@games.length
+      @games.length
     end
 
-    def find_by_id id
-      game = @@games[id]
-      if !game
-        raise "Game #{id} not found."
-      end
-      game
+    def find_by_id(id)
+      @games[id] || raise("Game #{id} not found.")
+    end
+
+    def []=(id, game)
+      @games[id] = game
     end
   end
 
-  def initialize( id:, board:, score:, game_over: )
+  def initialize(id:, board:, score:, game_over:)
     @id        = id
     @board     = board
     @score     = score
@@ -37,8 +40,6 @@ class Game
   end
 
   def save
-    @@games[@id] = self
+    Game[@id] = self
   end
-
-
 end
--- app/models/game/board.rb
+++ app/models/game/board.rb
@@ -1,37 +1,41 @@
-class Game::Board
-  private_class_method :new
+class Game
+  # Class representing the gameboard
+  # 2D grid of tiles which may be a bomb, number, or unknown
+  class Board
+    private_class_method :new
 
-  WIDTH = HEIGHT = SIZE = 5
+    WIDTH = HEIGHT = SIZE = 5
 
-  class << self
-    def random
-      weights = {
-        bomb: 3,
-        one:  4,
-        two:  2,
-        three: 1
-      }
+    WEIGHTS = {
+      bomb: 3,
+      one:  4,
+      two:  2,
+      three: 1
+    }.freeze
 
-      tiles = Array.new(HEIGHT) do
-        Array.new(WIDTH) do
-          Game::Tile.random(weights)
+    class << self
+      def random
+        tiles = Array.new(HEIGHT) do
+          Array.new(WIDTH) { Game::Tile.random(WEIGHTS) }
         end
+
+        revealed = Array.new(HEIGHT) do
+          Array.new(WIDTH) { Game::Tile.new(type: :hidden) }
+        end
+
+        new tiles: tiles, revealed: revealed
       end
-
-      revealed = Array.new(HEIGHT) { Array.new(WIDTH) { Game::Tile.new(type: :hidden) } }
-
-      new tiles: tiles, revealed: revealed
     end
-  end
 
-  def initialize(tiles:, revealed:)
-    @tiles    = tiles
-    @revealed = revealed
-  end
+    def initialize(tiles:, revealed:)
+      @tiles    = tiles
+      @revealed = revealed
+    end
 
-  def to_s
-    tiles = @tiles.map { |r| r.join(",") }.join("\n")
-    revealed = @revealed.map { |r| r.join(",") }.join("\n")
-    "#{tiles}\n\n#{revealed}"
+    def to_s
+      tiles = @tiles.map { |r| r.join(',') }.join("\n")
+      revealed = @revealed.map { |r| r.join(',') }.join("\n")
+      "#{tiles}\n\n#{revealed}"
+    end
   end
 end
