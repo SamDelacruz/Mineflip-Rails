3b7a6992733f3aef6db095d5ccbefdda9a23d8e2
Sam Delacruz <samdelacruz91@gmail.com>
2017-03-27 23:55:35 +0100

    Update cors for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
+Rails.application.config.middleware.insert_before 0, 'Rack::Cors' do
   allow do
-    origins 'localhost:3000'
-    resource '*',
+    origins 'localhost:3000', 'mineflip.com', 'www.mineflip.com'
+    resource(
+      '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
+    )
   end
 end
